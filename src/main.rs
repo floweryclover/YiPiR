fea09ef29495427f90709ac230568e9eea09d6e5
floweryclover <floweryclover@gmail.com>
2023-06-28 17:30:49 +0900

    actix_web 적용

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn hello(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(format!("hi {}", req_body))
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+    })
+        .bind(("127.0.0.1", 80))?
+        .run()
+        .await
 }
