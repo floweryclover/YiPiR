e96238dce98d5eeb62a66f01dadff94205812331
floweryclover <floweryclover@gmail.com>
2023-07-03 02:33:39 +0900

    get_recent_market_data() 실제 사용 목적으로 완성

--- src/upbit/public.rs
+++ src/upbit/public.rs
@@ -1,3 +1,6 @@
+use polars::prelude::*;
+use polars_io::prelude::*;
+use serde_json::error::Category::Data;
 use crate::upbit::{UPBitSocket, UPBitError, CallMethod, request_get, request_post, generate_request_body, response_to_json};
 
 impl UPBitSocket {
@@ -36,6 +39,52 @@ impl UPBitSocket {
                 } else {
                     return Ok(ticker_list);
                 }
+            }
+            Err(e) => Err(e)
+        }
+    }
+
+    // {
+    // "candle_acc_trade_price":106292470.96922,
+    // "candle_acc_trade_volume":2.61664687,
+    // "candle_date_time_kst":"2023-07-02T23:15:00",
+    // "candle_date_time_utc":"2023-07-02T14:15:00",
+    // "high_price":40630000.0,
+    // "low_price":40588000.0,"market":"KRW-BTC",
+    // "opening_price":40625000.0,
+    // "timestamp":1688307403765,
+    // "trade_price":40625000.0,
+    // "unit":5
+    // }
+    pub async fn get_recent_market_data(&self, ticker: &str, count: u8) -> Result<DataFrame, UPBitError> {
+        let url = format!("https://api.upbit.com/v1/candles/minutes/5?market={}&count={}", ticker, count);
+        match request_get(&self.reqwest_client, &url, CallMethod::Public).await {
+            Ok(json_array) => {
+
+                let mut basic_json = String::new();
+                for json in json_array {
+                    basic_json.push_str(json.to_string().as_str());
+                    basic_json.push_str("\n");
+                }
+                basic_json.pop();
+                println!("{}", basic_json);
+                let file = std::io::Cursor::new(basic_json);
+                let df = JsonReader::new(file)
+                    .with_json_format(JsonFormat::JsonLines)
+                    .finish()
+                    .unwrap()
+                    .select([
+                        "timestamp",
+                        "candle_date_time_kst",
+                        "high_price",
+                        "low_price",
+                        "opening_price",
+                        "trade_price",
+                    ])
+                    .unwrap()
+                    .sort(["timestamp"], false)
+                    .unwrap();
+                Ok(df)
             }
             Err(e) => Err(e)
         }
